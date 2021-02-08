e1b41c0e7139842c9b1be779857ffa29f73d6ecd
CaVaBang <musmag7@mail.ru>
2021-02-08 23:02:19 +0300

    добавил много нового

--- testles/conftest.py
+++ testles/conftest.py
@@ -4,16 +4,16 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='fr', help="Choose language: es, fr ...")
+    parser.addoption('--language', action='store', default='en', help="Choose language: es, fr ...")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
-    print("\nstart browser for test..")
+    print("\nСТАРТУЕМ")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
+    print("\nЗАКРУГЛЯЕМСЯ")
     browser.quit()
